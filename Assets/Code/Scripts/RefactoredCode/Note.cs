99d33aeeb85837b0b56b57d78ddec7f551faebdd
Natthapoom <natthapoom_insawan@cmu.ac.th>
2022-12-25 16:10:35 +0700

    Edited MoveToEndPoint function in Note.cs for furture modification

--- Assets/Code/Scripts/RefactoredCode/Note.cs
+++ Assets/Code/Scripts/RefactoredCode/Note.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace DyeTonic
 {
     public abstract class Note : MonoBehaviour
     {
         [SerializeField] SongManager _songManager;
+
+        //reach where it need to be hit
+        bool reachHitLine;
 
         public Transform StartTransform { get; set; }
         public Transform EndTransform { get; set; }
@@ -16,11 +20,27 @@ namespace DyeTonic
         protected virtual void Update()
         {
             MoveToEndPoint();
+
         }
 
         protected virtual void MoveToEndPoint()
         {
-            transform.position = Vector3.Lerp(StartTransform.position, EndTransform.position, (_songManager.songPosInBeats / NoteData.beat));
+            //move to end point
+            if (transform != EndTransform && !reachHitLine)
+                transform.position = Vector3.Lerp(StartTransform.position, EndTransform.position, (_songManager.songPosInBeats / NoteData.beat));
+
+            //reached end point
+            if (transform.position == EndTransform.position)
+            {
+                reachHitLine = true;
+                //Destroy(gameObject);
+                //Debug.Log("PERFECT");
+            }
+
+            //if (reachHitLine)
+            //    transform.position = Vector3.Lerp(endTransform.position, endTransform.position + new Vector3(0, 0, -2.5f), (songPosInBeats / (beat+10)));
+
+            //Debug.Log((songPosInBeats / (beat+10)).ToString());
         }
 
     }
