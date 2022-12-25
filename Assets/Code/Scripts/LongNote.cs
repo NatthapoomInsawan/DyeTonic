f0fabab0355bee9b4c543a5c30271c66433fbb33
Natthapoom <natthapoom_insawan@cmu.ac.th>
2022-12-25 18:20:25 +0700

    Edited LongNote script and Note Spawner script for spawning long Note
    Edited Scene view for testing
    
    Edited WorldIsMineSD for testing

--- Assets/Code/Scripts/LongNote.cs
+++ Assets/Code/Scripts/LongNote.cs
@@ -7,18 +7,31 @@ namespace DyeTonic
     [RequireComponent(typeof(LineRenderer))]
     public class LongNote : Note
     {
-        public LineRenderer LineRenderer { get; set; }
+
+        LineRenderer lineRenderer;
+
+        public Transform TailNoteTransform { get; set; }
 
         // Start is called before the first frame update
         void Start()
         {
-            LineRenderer = GetComponent<LineRenderer>();
+            lineRenderer = GetComponent<LineRenderer>();
         }
 
         // Update is called once per frame
         protected override void Update()
         {
             base.Update();
+
+            DrawLine();
         }
+
+        //Draw line between two head and tail
+        public void DrawLine()
+        {
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, TailNoteTransform.position);
+        }
+
     }
 }
