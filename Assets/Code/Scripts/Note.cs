0c83dfbbd80f0f578a111a60c2d50be4c08efe99
Natthapoom <natthapoom_insawan@cmu.ac.th>
2022-12-29 02:42:27 +0700

    Edit Note script for visualize note passing hit line

--- Assets/Code/Scripts/Note.cs
+++ Assets/Code/Scripts/Note.cs
@@ -9,9 +9,6 @@ namespace DyeTonic
     {
         [SerializeField] SongManager _songManager;
 
-        //reach where it need to be hit
-        bool reachHitLine;
-
         public Transform StartTransform { get; set; }
         public Transform EndTransform { get; set; }
         public NoteData NoteData { get; set; }
@@ -25,22 +22,13 @@ namespace DyeTonic
 
         protected virtual void MoveToEndPoint()
         {
-            //move to end point
-            if (transform != EndTransform && !reachHitLine)
-                transform.position = Vector3.Lerp(StartTransform.position, EndTransform.position, (_songManager.songPosInBeats / NoteData.beat));
+            //move to hit    point
+            transform.position = Vector3.LerpUnclamped(StartTransform.position, EndTransform.position, (_songManager.songPosInBeats / NoteData.beat));
 
-            //reached end point
-            if (transform.position == EndTransform.position)
-            {
-                reachHitLine = true;
-                //Destroy(gameObject);
-                //Debug.Log("PERFECT");
-            }
+            //Destroy when pass or equal 1.5 beat
+            if ((_songManager.songPosInBeats - NoteData.beat) >= 1.5f)
+                Destroy(gameObject);
 
-            //if (reachHitLine)
-            //    transform.position = Vector3.Lerp(endTransform.position, endTransform.position + new Vector3(0, 0, -2.5f), (songPosInBeats / (beat+10)));
-
-            //Debug.Log((songPosInBeats / (beat+10)).ToString());
         }
 
     }
