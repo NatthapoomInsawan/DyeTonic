54fd15da7ef6c718b6e09c2d262e8d910f4b945e
Natthapoom <natthapoom_insawan@cmu.ac.th>
2023-01-20 03:30:04 +0700

    Edited LevelEditorSongPlayer to update timeline in LevelEditor Scene

--- Assets/Code/Scripts/LevelEditor/LevelEditorSongPlayer.cs
+++ Assets/Code/Scripts/LevelEditor/LevelEditorSongPlayer.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 namespace DyeTonic
 {
@@ -10,6 +12,11 @@ namespace DyeTonic
         [SerializeField] SongManager _songManager;
         [SerializeField] SongData _songData;
 
+        [Header("UI Objects referencing")]
+        [SerializeField] TextMeshProUGUI _textBox;
+        [SerializeField] Slider _timelineSlider;
+        [SerializeField] TextMeshProUGUI _playButtonText;
+
         [Header("Song Information")]
         //the duration of a beat
         [SerializeField] float secPerBeat;
@@ -18,8 +25,14 @@ namespace DyeTonic
         [SerializeField] float songPosition;
 
         //Song position in beat to show
-
         [SerializeField] float songPositionInBeats;
+
+        float oldSongPosition;
+        float changeValue;
+
+        bool songPlay = false;
+
+        AudioSource audioSource;
 
         //how much time (in seconds) has passed since the song started
         float dspSongTime;
@@ -48,35 +61,143 @@ namespace DyeTonic
 
             //reset song HP
             _songManager.HP = 100;
+
+            //set up AudioSource
+            audioSource = GetComponent<AudioSource>();
         }
 
         // Start is called before the first frame update
         void Start()
         {
-            //calculate how many seconds is one beat
-            secPerBeat = 60f / _songData.songBpm;
-
             //record the time when the song starts
             dspSongTime = (float)AudioSettings.dspTime;
 
-            //start the song
-            AudioSource audioSource = GetComponent<AudioSource>();
-            audioSource.clip = _songData.song;
-            audioSource.Play();
+            //load song
+            LoadSong();
+
+            //play song
+            PlaySong();
 
         }
 
         // Update is called once per frame
         void Update()
         {
-            //calculate the position in seconds
-            songPosition = (float)(AudioSettings.dspTime - dspSongTime);
+            if (songPlay)
+            {
+                //calculate the position in seconds
+                songPosition = (float)(AudioSettings.dspTime - dspSongTime);
 
-            //calculate the position in beats
-            _songManager.songPosInBeats = songPosition / secPerBeat;
+                //calculate the position in beats
+                _songManager.songPosInBeats = songPosition / secPerBeat;
 
-            songPositionInBeats = _songManager.songPosInBeats;
+                songPositionInBeats = _songManager.songPosInBeats;
+
+                //update timeline slider
+                _timelineSlider.value = songPosition;
+
+                //update old song position
+                oldSongPosition = songPosition;
+
+            }
+            else
+                dspSongTime = (float)AudioSettings.dspTime;
+
+            UpdateUI();
 
         }
+
+        void LoadSong()
+        {
+            //calculate how many seconds is one beat
+            secPerBeat = 60f / _songData.songBpm;
+
+            audioSource.clip = _songData.song;
+
+            _timelineSlider.maxValue = audioSource.clip.length;
+        }
+
+        private void PlaySong()
+        {
+
+            audioSource.Play();
+            songPlay = true;
+        }
+
+        private void UnPauseSong()
+        {
+            dspSongTime = (float)AudioSettings.dspTime - oldSongPosition;
+
+            if (changeValue != 0)
+            {
+                dspSongTime -= changeValue;
+                audioSource.time = songPosition;
+                audioSource.Play();
+            }
+            else
+                audioSource.UnPause();
+
+            songPlay = true;
+            changeValue = 0;
+        }
+
+        private void PauseSong()
+        {
+            audioSource.Pause();
+            songPlay = false;
+        }
+
+        void UpdateUI()
+        {
+            _textBox.text = "Song name: " +_songData.name + "\n" +
+                           "BPM: " + _songData.songBpm + "\n" +
+                           "Sec per beat: " + secPerBeat + "\n" +
+                           "Song position in sec: " + songPosition + "\n" +
+                           "Song position in beat: " + songPositionInBeats;
+
+        }
+
+        public void OnPlayButton()
+        {
+            if (songPlay)
+            {
+                _playButtonText.text = "Play";
+                PauseSong();
+            }
+            else 
+            {
+                _playButtonText.text = "Pause";
+                UnPauseSong();
+            }
+
+        }
+
+        public void OnTimelineSliderChange()
+        {
+
+            if (!songPlay)
+            {
+                songPosition = _timelineSlider.value;
+
+                ////calculate the position in beats
+                _songManager.songPosInBeats = songPosition / secPerBeat;
+
+                songPositionInBeats = _songManager.songPosInBeats;
+
+                dspSongTime = (float)AudioSettings.dspTime - oldSongPosition;
+
+                songPosition = (float)AudioSettings.dspTime - dspSongTime;
+
+                changeValue = _timelineSlider.value - songPosition;
+
+                songPosition = _timelineSlider.value;
+
+            }
+
+            //debug.log("dsp time:" + audiosettings.dsptime);
+            //debug.log("timer stamp:" + dspsongtime);
+            //debug.log("difference:" + (audiosettings.dsptime - dspsongtime));
+        }
+
     }
 }
