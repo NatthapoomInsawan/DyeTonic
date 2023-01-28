0ffc7bcef50a3833fa882db0fb7effa60d280c9d
Natthapoom <natthapoom_insawan@cmu.ac.th>
2023-01-29 02:09:44 +0700

    Created SongPanel script

---
+++ Assets/Code/Scripts/UI/SongPanel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DyeTonic
+{
+    public class SongPanel : SongCoverDisplay
+    {
+
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
