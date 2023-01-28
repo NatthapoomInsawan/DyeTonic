3b34c9d06c7e9896d2f135a55024cbf24b48c89a
Natthapoom <natthapoom_insawan@cmu.ac.th>
2023-01-29 00:53:32 +0700

    Edited SongListLoader script

--- Assets/Code/Scripts/UI/SongListLoader.cs
+++ Assets/Code/Scripts/UI/SongListLoader.cs
@@ -7,16 +7,22 @@ namespace DyeTonic
     public class SongListLoader : MonoBehaviour
     {
         [SerializeField] private SongData[] songDatas;
+        [SerializeField] private GameObject songButtonPrefab;
+        [SerializeField] private Transform contentContainer;
 
         private void Awake()
         {
-            songDatas = Resources.LoadAll<SongData>("Resources/");
+            songDatas = Resources.LoadAll<SongData>("SongData");
         }
 
         // Start is called before the first frame update
         void Start()
         {
-        
+            for (int i = 0; i < songDatas.Length; i++)
+            {
+                var instantiateObject = Instantiate(songButtonPrefab, contentContainer);
+                instantiateObject.name = songDatas[i].name;
+            }
         }
     }
 }
