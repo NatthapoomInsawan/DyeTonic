93d04c021563037df92827c60344c3bef7b51756
Natthapoom <natthapoom_insawan@cmu.ac.th>
2023-02-13 21:32:40 +0700

    Created RoomSelectButton script and assigned to RoomSelectButton prefab

---
+++ Assets/Code/Scripts/UI/RoomSelectButton.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace DyeTonic
+{
+    public class RoomSelectButton : SongCoverDisplay
+    {
+        [SerializeField] private TextMeshProUGUI roomNameText;
+        public string RoomName { get; set; }
+
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        // Update is called once per frame
+        public void OnButtonClick()
+        {
+
+        }
+
+        public override void UpdateSongCoverDisplay(SongData songData)
+        {
+            _songData = songData;
+
+            //song name text
+            if (_songData.songName != null || _songData.songName == "")
+                songNameText.text = _songData.songName;
+            else
+                songNameText.text = _songData.name;
+
+            //artist text
+            if (_songData.songArtist != null)
+                artistNameText.text = _songData.songArtist;
+            else
+                artistNameText.text = "";
+
+            //song Image
+            if (songImage != null)
+                songImage.sprite = _songData.songSprite;
+
+            //set room name
+            roomNameText.text = RoomName;
+
+        }
+
+    }
+}
