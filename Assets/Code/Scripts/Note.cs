5e2620f8b9b86a800a61bdf398248969540b3597
Natthapoom Insawan <natthapoom_insawan@cmu.ac.th>
2023-02-27 10:53:55 +0000

    Revert "Merge branch 'Art' into 'main'"
    This reverts merge request !70

--- Assets/Code/Scripts/Note.cs
+++ Assets/Code/Scripts/Note.cs
@@ -16,7 +16,7 @@ namespace DyeTonic
         public NoteData NoteData { get; set; }
 
         //OnNoteSelfDestroy
-        public static event Action OnNoteSelfDestroy;
+        public static event Action<Transform, NoteData> OnNoteSelfDestroy;
 
         // Update is called once per frame
         protected virtual void Update()
@@ -41,7 +41,7 @@ namespace DyeTonic
 
         protected void InVokeSelfDestroy()
         {
-            OnNoteSelfDestroy?.Invoke();
+            OnNoteSelfDestroy?.Invoke(EndTransform, NoteData);
         }
 
     }
