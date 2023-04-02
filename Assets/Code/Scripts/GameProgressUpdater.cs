083279f24b2ba2db7444ffed3389285a463f3aed
Natthapoom Insawan <natthapoom_insawan@cmu.ac.th>
2023-04-02 03:14:13 +0000

    Added story mode ending, Fixed GameOverScene scripts  Fixed notes self destroy glitch

--- Assets/Code/Scripts/GameProgressUpdater.cs
+++ Assets/Code/Scripts/GameProgressUpdater.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace DyeTonic
 {
@@ -16,7 +17,7 @@ namespace DyeTonic
         // Start is called before the first frame update
         void Start()
         {
-            if (_songManager.IsStoryMode())
+            if (_songManager.IsStoryMode() && _songManager.HP != 0)
                 UpdateStoryProgress();
             else
                 gameObject.SetActive(false);
@@ -24,11 +25,23 @@ namespace DyeTonic
 
         private void UpdateStoryProgress()
         {
+            if (_gameSettings.progress >= _songList.Count)
+                return;
+
             if (_songManager.GetCurrentSongData() == _songList[_gameSettings.progress])
             {
                 _gameSettings.progress++;
+
                 _gameSettings.SaveData();
             }
+        }
+
+        public void OnStageClear()
+        {
+            if (_songManager.IsStoryMode() && _gameSettings.progress == 3 && _songManager.HP != 0)
+                SceneManager.LoadScene("Chapter 3_2");
+            else
+                SceneLoader.LoadPreviousScene();
         }
     }
 }
